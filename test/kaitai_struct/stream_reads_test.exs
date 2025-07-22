72f2294070a55052af9aba3cd9a629100763b77d
Cory Finger <cory.j.finger@gmail.com>
2025-07-21 23:36:24 -0700

    Add tests for unsigned integers

--- test/kaitai_struct/stream_reads_test.exs
+++ test/kaitai_struct/stream_reads_test.exs
@@ -248,6 +248,258 @@ defmodule KaitaiStruct.StreamReadsTest do
     end
   end
 
+
+
+
+
+
+
+  test "read_u1/1 reads a byte properly" do
+    stream = binary_stream(<<255::unsigned-integer-8>>)
+    assert {:ok, 255} = KaitaiStruct.Stream.read_u1(stream)
+
+    stream = binary_stream(<<4::unsigned-integer-8, 5::unsigned-integer-8, 6::unsigned-integer-8>>)
+    assert {:ok, 4} = KaitaiStruct.Stream.read_u1(stream)
+    assert {:ok, 5} = KaitaiStruct.Stream.read_u1(stream)
+    assert {:ok, 6} = KaitaiStruct.Stream.read_u1(stream)
+  end
+
+  test "read_u1/1 responds with error on EOF" do
+    stream = binary_stream(<<4::unsigned-integer-8>>)
+    assert {:ok, 4} = KaitaiStruct.Stream.read_u1(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u1(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u1(stream)
+  end
+
+  test "read_u1!/1 reads a byte properly" do
+    stream = binary_stream(<<255::unsigned-integer-8>>)
+    assert 255 = KaitaiStruct.Stream.read_u1!(stream)
+
+    stream = binary_stream(<<4::unsigned-integer-8, 5::unsigned-integer-8, 6::unsigned-integer-8>>)
+    assert 4 = KaitaiStruct.Stream.read_u1!(stream)
+    assert 5 = KaitaiStruct.Stream.read_u1!(stream)
+    assert 6 = KaitaiStruct.Stream.read_u1!(stream)
+  end
+
+  test "read_u1!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<4::unsigned-integer-8>>)
+    assert 4 = KaitaiStruct.Stream.read_u1!(stream)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_u1!(stream)
+    end
+  end
+
+  test "read_u2be/1 reads 2 bytes properly" do
+    stream = binary_stream(<<65_535::unsigned-integer-big-16>>)
+    assert {:ok, 65_535} = KaitaiStruct.Stream.read_u2be(stream)
+
+    stream = binary_stream(<<1042::unsigned-integer-big-16, 6232::unsigned-integer-big-16>>)
+    assert {:ok, 1042} = KaitaiStruct.Stream.read_u2be(stream)
+    assert {:ok, 6232} = KaitaiStruct.Stream.read_u2be(stream)
+  end
+
+  test "read_u2be/1 responds with error on EOF" do
+    stream = binary_stream(<<255::unsigned-integer-big-8>>)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u2be(stream)
+
+    stream = binary_stream(<<592::unsigned-integer-big-16, 6232::unsigned-integer-big-8>>)
+    assert {:ok, 592} = KaitaiStruct.Stream.read_u2be(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u2be(stream)
+  end
+
+  test "read_u2be!/1 reads 2 bytes properly" do
+    stream = binary_stream(<<65_534::unsigned-integer-big-16>>)
+    assert 65_534 = KaitaiStruct.Stream.read_u2be!(stream)
+
+    stream = binary_stream(<<1042::unsigned-integer-big-16, 6232::unsigned-integer-big-16>>)
+    assert 1042 = KaitaiStruct.Stream.read_u2be!(stream)
+    assert 6232 = KaitaiStruct.Stream.read_u2be!(stream)
+  end
+
+  test "read_u2be!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<423::unsigned-integer-big-8>>)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_u2be!(stream)
+    end
+  end
+
+  test "read_u4be/1 reads 4 bytes properly" do
+    stream = binary_stream(<<4_294_967_295::unsigned-integer-big-32>>)
+    assert {:ok, 4_294_967_295} = KaitaiStruct.Stream.read_u4be(stream)
+
+    stream = binary_stream(<<11_717_216::unsigned-integer-big-32, 12_774_216::unsigned-integer-big-32>>)
+    assert {:ok, 11_717_216} = KaitaiStruct.Stream.read_u4be(stream)
+    assert {:ok, 12_774_216} = KaitaiStruct.Stream.read_u4be(stream)
+  end
+
+  test "read_u4be/1 responds with error on EOF" do
+    stream = binary_stream(<<823::unsigned-integer-big-16>>)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u4be(stream)
+
+    stream = binary_stream(<<4324::unsigned-integer-big-32, 42123::unsigned-integer-big-8>>)
+    assert {:ok, 4324} = KaitaiStruct.Stream.read_u4be(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u4be(stream)
+  end
+
+  test "read_u4be!/1 reads 4 bytes properly" do
+    stream = binary_stream(<<82_994_923::unsigned-integer-big-32>>)
+    assert 82_994_923 = KaitaiStruct.Stream.read_u4be!(stream)
+
+    stream = binary_stream(<<1042::unsigned-integer-big-32, 64_432_232::unsigned-integer-big-32>>)
+    assert 1042 = KaitaiStruct.Stream.read_u4be!(stream)
+    assert 64_432_232 = KaitaiStruct.Stream.read_u4be!(stream)
+  end
+
+  test "read_u4be!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<4262233::unsigned-integer-big-16>>)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_s4be!(stream)
+    end
+  end
+
+  test "read_u8be/1 reads 8 bytes properly" do
+    stream = binary_stream(<<18_446_744_073_709_551_615::unsigned-integer-big-64>>)
+    assert {:ok, 18_446_744_073_709_551_615} = KaitaiStruct.Stream.read_u8be(stream)
+
+    stream = binary_stream(<<281_474_976_710_656::unsigned-integer-big-64, 42::unsigned-integer-big-64>>)
+    assert {:ok, 281_474_976_710_656} = KaitaiStruct.Stream.read_u8be(stream)
+    assert {:ok, 42} = KaitaiStruct.Stream.read_u8be(stream)
+  end
+
+  test "read_u8be/1 responds with error on EOF" do
+    stream = binary_stream(<<823::unsigned-integer-big-32>>)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u8be(stream)
+
+    stream = binary_stream(<<543::unsigned-integer-big-64, -42123::unsigned-integer-big-32>>)
+    assert {:ok, 543} = KaitaiStruct.Stream.read_u8be(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u8be(stream)
+  end
+
+  test "read_u8be!/1 reads 8 bytes properly" do
+    stream = binary_stream(<<391_474_976_710_456::unsigned-integer-big-64>>)
+    assert 391_474_976_710_456 = KaitaiStruct.Stream.read_u8be!(stream)
+
+    stream = binary_stream(<<2041::unsigned-integer-big-64, 64_431_232::unsigned-integer-big-64>>)
+    assert 2041 = KaitaiStruct.Stream.read_u8be!(stream)
+    assert 64_431_232 = KaitaiStruct.Stream.read_u8be!(stream)
+  end
+
+  test "read_u8be!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<4_262_233::unsigned-integer-big-32>>)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_u8be!(stream)
+    end
+  end
+
+  test "read_u2le/1 reads 2 bytes properly" do
+    stream = binary_stream(<<423::unsigned-integer-little-16>>)
+    assert {:ok, 423} = KaitaiStruct.Stream.read_u2le(stream)
+
+    stream = binary_stream(<<1042::unsigned-integer-little-16, 6232::unsigned-integer-little-16>>)
+    assert {:ok, 1042} = KaitaiStruct.Stream.read_u2le(stream)
+    assert {:ok, 6232} = KaitaiStruct.Stream.read_u2le(stream)
+  end
+
+  test "read_u2le/1 responds with error on EOF" do
+    stream = binary_stream(<<423::unsigned-integer-little-8>>)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u2le(stream)
+
+    stream = binary_stream(<<592::unsigned-integer-little-16, 6232::unsigned-integer-little-8>>)
+    assert {:ok, 592} = KaitaiStruct.Stream.read_u2le(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u2le(stream)
+  end
+
+  test "read_u2le!/1 reads 2 bytes properly" do
+    stream = binary_stream(<<423::unsigned-integer-little-16>>)
+    assert 423 = KaitaiStruct.Stream.read_u2le!(stream)
+
+    stream = binary_stream(<<1042::unsigned-integer-little-16, 6232::unsigned-integer-little-16>>)
+    assert 1042 = KaitaiStruct.Stream.read_u2le!(stream)
+    assert 6232 = KaitaiStruct.Stream.read_u2le!(stream)
+  end
+
+  test "read_u2le!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<423::unsigned-integer-little-8>>)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_u2le!(stream)
+    end
+  end
+
+  test "read_u4le/1 reads 4 bytes properly" do
+    stream = binary_stream(<<16_777_216::unsigned-integer-little-32>>)
+    assert {:ok, 16_777_216} = KaitaiStruct.Stream.read_u4le(stream)
+
+    stream = binary_stream(<<11_717_216::unsigned-integer-little-32, 12_774_216::unsigned-integer-little-32>>)
+    assert {:ok, 11_717_216} = KaitaiStruct.Stream.read_u4le(stream)
+    assert {:ok, 12_774_216} = KaitaiStruct.Stream.read_u4le(stream)
+  end
+
+  test "read_u4le/1 responds with error on EOF" do
+    stream = binary_stream(<<823::unsigned-integer-little-16>>)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u4le(stream)
+
+    stream = binary_stream(<<4324::unsigned-integer-little-32, 42123::unsigned-integer-little-8>>)
+    assert {:ok, 4324} = KaitaiStruct.Stream.read_u4le(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u4le(stream)
+  end
+
+  test "read_u4le!/1 reads 4 bytes properly" do
+    stream = binary_stream(<<82_994_923::unsigned-integer-little-32>>)
+    assert 82_994_923 = KaitaiStruct.Stream.read_u4le!(stream)
+
+    stream = binary_stream(<<1042::unsigned-integer-little-32, 64_432_232::unsigned-integer-little-32>>)
+    assert 1042 = KaitaiStruct.Stream.read_u4le!(stream)
+    assert 64_432_232 = KaitaiStruct.Stream.read_u4le!(stream)
+  end
+
+  test "read_u4le!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<4262233::unsigned-integer-little-16>>)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_u4le!(stream)
+    end
+  end
+
+  test "read_u8le/1 reads 8 bytes properly" do
+    stream = binary_stream(<<281_474_976_710_656::unsigned-integer-little-64>>)
+    assert {:ok, 281_474_976_710_656} = KaitaiStruct.Stream.read_u8le(stream)
+
+    stream = binary_stream(<<281_474_976_710_656::unsigned-integer-little-64, 42::unsigned-integer-little-64>>)
+    assert {:ok, 281_474_976_710_656} = KaitaiStruct.Stream.read_u8le(stream)
+    assert {:ok, 42} = KaitaiStruct.Stream.read_u8le(stream)
+  end
+
+  test "read_u8le/1 responds with error on EOF" do
+    stream = binary_stream(<<823::unsigned-integer-little-32>>)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u8le(stream)
+
+    stream = binary_stream(<<543::unsigned-integer-little-64, 42123::unsigned-integer-little-32>>)
+    assert {:ok, 543} = KaitaiStruct.Stream.read_u8le(stream)
+    assert {:error, :reached_eof} = KaitaiStruct.Stream.read_u8le(stream)
+  end
+
+  test "read_u8le!/1 reads 8 bytes properly" do
+    stream = binary_stream(<<391_474_976_710_456::unsigned-integer-little-64>>)
+    assert 391_474_976_710_456 = KaitaiStruct.Stream.read_u8le!(stream)
+
+    stream = binary_stream(<<2041::unsigned-integer-little-64, 64_431_232::unsigned-integer-little-64>>)
+    assert 2041 = KaitaiStruct.Stream.read_u8le!(stream)
+    assert 64_431_232 = KaitaiStruct.Stream.read_u8le!(stream)
+  end
+
+  test "read_u8le!/1 raises ReadError on EOF" do
+    stream = binary_stream(<<4262233::unsigned-integer-little-32>>)
+
+    assert_raise KaitaiStruct.Stream.ReadError, fn ->
+      KaitaiStruct.Stream.read_u8le!(stream)
+    end
+  end
+
   defp binary_stream(bin_data) do
     io_stream = StringIO.open(bin_data) |> then(fn {:ok, io} -> IO.binstream(io, 1) end)
     {:ok, kaitai} = GenServer.start_link(KaitaiStruct.Stream, {io_stream, byte_size(bin_data)})
